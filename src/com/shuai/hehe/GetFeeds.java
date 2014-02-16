a40b189515617ea0754573372006438cc31cb8d7
shuaiweican <49662990@qq.com>
2014-02-17 00:23:25 +0800

    1.更新文档，更新服务

--- src/com/shuai/hehe/GetFeeds.java
+++ src/com/shuai/hehe/GetFeeds.java
@@ -1,6 +1,11 @@
 package com.shuai.hehe;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -32,13 +37,48 @@ public class GetFeeds extends HttpServlet {
 	 */
 	public void destroy() {
 	}
+	
+	private Connection getConnection() throws SQLException{
+		Connection connection = DriverManager.getConnection("jdbc:sqlite:D:/mycode/hehe.db");
+		return connection;
+	}
+	
+	private void closeConnection(Connection connection){
+		try {
+			if (connection != null){
+				connection.close();
+//				connection=null;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().print("xxxx");
+		String idStr=request.getParameter("id");		
+		int id=-1;
+		try{
+			id=Integer.parseInt(idStr);
+		}catch(NumberFormatException ex){
+			
+		}
+		
+		String countString=request.getParameter("count");
+		int count=20;
+		try{
+			count=Integer.parseInt(countString);
+		}catch(NumberFormatException ex){
+			
+		}
+		
+		
+		
+		
+//		response.getWriter().print("xxxx");
 	}
 
 	/**
