38d130483b011be331ebadc3bbda690cb983db47
shuaiweican <49662990@qq.com>
2014-06-13 20:41:41 +0800

    1.更新sql

--- sql.sql
+++ sql.sql
@@ -9,3 +9,11 @@ CREATE TABLE IF NOT EXISTS hot_album(id INT PRIMARY KEY,type INT,`title` VARCHAR
 
 #dump data
 mysqldump --database hehe --user=hot_feed_user --password=test  --skip-triggers --compact --no-create-info --tables hot_feed --where="id<3" > "D:/TEST/TEST.SQL"
+
+UPDATE hehe.hot_feed set show_time=show_time+INTERVAL 6 Hour where id>240;
+
+#update show_time
+UPDATE hot_feed set show_time=TIMESTAMPADD(MINUTE,TIMESTAMPDIFF(MINUTE,'2014-06-19 08:03:31','2014-06-19 12:03:31'),show_time) where id>240;
+UPDATE hot_feed SET show_time = DATE_ADD(show_time, INTERVAL 8 Hour)  where id>240;
+
+select DATE_ADD(show_time, INTERVAL 1 MINUTE) from hehe.hot_feed where id>240;
