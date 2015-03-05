731603bbdd55eab6281f9d0f3223c95feb094eb3
shuaiweican <49662990@qq.com>
2015-03-05 16:16:12 +0800

    1.增加uc渠道测试接口

---
+++ src/com/anysdk/auth/UcConstant.java
@@ -0,0 +1,11 @@
+package com.anysdk.auth;
+
+
+public class UcConstant {
+    public static final boolean DEBUG=true;
+    public static final String UC_CPID="51552";
+    public static final String UC_GAME_ID="552889";
+    public static final String UC_SERVER_ID="1";
+    public static final String UC_API_KEY="1fb78942404d4f9c6482240a5d524e91";
+    
+}
--- src/com/shuai/hehe/server/Log.java
+++ src/com/shuai/hehe/server/Log.java
@@ -3,7 +3,29 @@ package com.shuai.hehe.server;
 import java.util.Date;
 
 public class Log {
+	
+	public static void debug(String log){
+		debug("",log);
+    }
+	
+	public static void debug(String tag,String log){
+        System.out.println(new Date().toString()+" " + tag +log);
+    }
+	
+	
+	public static void info(String log){
+		info("",log);
+    }
+	
     public static void info(String tag,String log){
+        System.out.println(new Date().toString()+" " + tag +log);
+    }
+    
+    public static void error(String log){
+    	error("",log);
+    }
+    
+    public static void error(String tag,String log){
         System.out.println(new Date().toString()+" " + tag +log);
     }
 
